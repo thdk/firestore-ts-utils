763776f11a66687ef77c0060388265abf54f9128
thdk <t.dekiere@gmail.com>
2019-10-10 22:44:18 +0200

    Add getManyAsync function

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { DocumentData, QueryDocumentSnapshot, DocumentSnapshot, CollectionReference, UpdateData } from "@firebase/firestore-types";
 
-export function typeSnapshot<T extends DocumentData>(snapshot: QueryDocumentSnapshot): T;
-export function typeSnapshot<T extends DocumentData>(snapshot: DocumentSnapshot): T | undefined;
-export function typeSnapshot<T extends DocumentData>(snapshot: DocumentSnapshot | QueryDocumentSnapshot): T | undefined {
+
+export function typeSnapshot<T extends DocumentData>(snapshot: QueryDocumentSnapshot): T & { id: string };
+export function typeSnapshot<T extends DocumentData>(snapshot: DocumentSnapshot): (T & { id: string }) | undefined;
+export function typeSnapshot<T extends DocumentData>(snapshot: DocumentSnapshot | QueryDocumentSnapshot): (T & { id: string }) | undefined {
     const docData = snapshot.data();
     if (docData === undefined) return undefined;
-    else return Object.assign<Pick<T, "id">, Exclude<T, "id">>({ id: snapshot.id }, docData as Exclude<T, "id">);
+    else return Object.assign({ id: snapshot.id }, docData as T);
 }
 
 export const updateAsync = <T extends UpdateData>(collectionRef: CollectionReference, data: Partial<T> & Pick<T, "id">) => {
@@ -20,11 +21,17 @@ export const addAsync = <T>(collectionRef: CollectionReference, data: Partial<T>
 }
 
 export const getAsync = <T>(collectionRef: CollectionReference, id: string) => {
-    return new Promise<T>((resolve, reject) => {
+    return new Promise<T & { id: string }>((resolve, reject) => {
         collectionRef.doc(id).get()
             .then((d: DocumentSnapshot) => d.data()
                 ? resolve(typeSnapshot<T>(d))
                 : reject()
             );
     });
+}
+
+export const getManyAsync = <T>(collectionRef: CollectionReference, ...ids: string[]) => {
+    return Promise.all(
+        ids.map(id => getAsync<T>(collectionRef, id))
+    );
 }
